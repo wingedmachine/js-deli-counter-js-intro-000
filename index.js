a657714e330c31eb21abe3e21db3f8fde8c80f93
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-14 20:17:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ function currentLine(line) {
   var lineIsCurrently = 'The line is currently';
   if(line.length > 0) {
     for(var i = 0; i < line.length; i++) {
-      lineIsCurrently += i === 0 ? ": " : ", "
-      lineIsCurrently += `${i+1}. ${line[i]}`,
+      lineIsCurrently += i === 0 ? ": " : ", ";
+      lineIsCurrently += `${i+1}. ${line[i]}`;
     }
   } else {
     return lineIsCurrently + " empty.";
