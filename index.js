f479af2b1b0010635e5ed6c38de500f175af2959
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-14 20:16:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ function takeANumber(line, name) {
 function nowServing(line) {
   return line.length > 0 ? `Currently serving ${line.shift()}.` : "There is nobody waiting to be served!";
 }
+
+function currentLine(line) {
+  var lineIsCurrently = 'The line is currently';
+  if(line.length > 0) {
+    for(var i = 0; i < line.length; i++) {
+      lineIsCurrently += i === 0 ? ": " : ", "
+      lineIsCurrently += `${i+1}`. {line[i]}, 
+    }
+  } else {
+    return lineIsCurrently + " empty.";
+  }
+}
